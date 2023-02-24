a5f617b9c6301519082131dff82bfa1f49ed4a37
Anurag-Varma <johndoe@example.companuragvarma123456789@gmail.com>
2023-02-25 00:33:30 +0530

    added episodes to each anime via api

--- frontend/src/Content/RecentlyUpdated/RecentlyUpdated.js
+++ frontend/src/Content/RecentlyUpdated/RecentlyUpdated.js
@@ -1,14 +1,15 @@
 import React, {useState,useEffect} from 'react';
 import { Card, Row, Col} from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import './RecentlyUpdated.css';
 
 const RecentlyUpdated = () => {
 
   const [recentAnime, setRecentAnime] = useState([
-    {"isSimulcast":true,
-    "isSubbed":true,
-    "isDubbed":true,
-    "year":2023,
+    {"isSimulcast":false,
+    "isSubbed":false,
+    "isDubbed":false,
+    "year":0,
     "slug":"",
     "episodeNumber":0,
     "title":"",
@@ -23,31 +24,36 @@ const RecentlyUpdated = () => {
                     "height":1536
                     }
             },
-    "lastUpdate":"2023-02-22T21:03:07.000Z",
+    "lastUpdate":"",
     "updatedString":""
     }
   ]);
-  var link = 'https://www2.kickassanime.ro/api/recent_update?episodeType=all&page=0&perPage=18';
+  var recentlyUpdatedLink = 'https://www2.kickassanime.ro/api/recent_update?episodeType=all&page=0&perPage=18';
 
   useEffect(() => {
     
-    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(link)}&nocache=${Date.now()}`)
+    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(recentlyUpdatedLink)}&nocache=${Date.now()}`)
     .then(response => {
 	   if (response.ok) {
-      console.log(response);
       return response.json()
     }
 	    throw new Error('Network response was not ok.')
     })
     .then(data => {
-      console.log(data);
       var finalData = JSON.parse(data.contents);
       setRecentAnime(finalData);
     });
 
   }, []);
 
-  console.log(recentAnime);
+  const navigate = useNavigate();
+  const handleEpisodeView = (anime) => {
+    navigate('/EpisodeView',{
+      state: {slug: anime.slug}
+    });
+
+  };
+
 
   return (
     <div className="recently-updated">
@@ -59,7 +65,7 @@ const RecentlyUpdated = () => {
           
           {recentAnime.map((anime, idx) => (
             <Col xs={6} sm={4} md={3} lg={2} key={idx}>
-              <Card>
+              <Card onClick={()=>handleEpisodeView(anime)}>
                 <Card.Img variant="top" src={anime.poster.hq.name!=="anime-hq"?`https://www2.kickassanime.ro/images/poster/${anime.poster.hq.name}.${anime.poster.hq.formats[1]}`:""} />
                 <Card.Body style={{padding:"0.5em"}}>
                   <Card.Title>{anime.title}</Card.Title>
--- frontend/src/Pages/EpisodeView/EpisodeView.js
+++ frontend/src/Pages/EpisodeView/EpisodeView.js
@@ -1,6 +1,60 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import './EpisodeView.css';
 
-function EpisodeView() {
+function EpisodeView({anime}) {
+
+    let location = useLocation();;
+    let animeWatchLink = `https://www2.kickassanime.ro/api/watch/${location.state.slug}`;
+   
+    const [animeWatch, setAnimeWatch] = useState([
+      {"anime_id":"",
+      "name":"",
+      "title":"",
+      "description":"",
+      "seasonId":"",
+      "seasonNumber":0,
+      "episodeNumber":0,
+      "banner":{"sm":{"name":"anime-sm","formats":["jpeg","webp"],"width":853,"height":480},
+                "hq":{"name":"anime-hq","formats":["jpeg","webp"],"width":1820,"height":1024}
+              },
+      "poster":{"sm":{"name":"anime-sm","formats":["jpeg","webp"],"width":480,"height":720},
+                "hq":{"name":"anime-hq","formats":["jpeg","webp"],"width":1024,"height":1536}
+              },
+      "thumbnail":{"sm":{"name":"anime-sm","formats":["jpeg","webp"],"width":853,"height":480},
+                    "hq":{"name":"anime-hq","formats":["jpeg","webp"],"width":1820,"height":1024}
+                  },
+      "lang":"",
+      "genres":[""],
+      "year":0,
+      "maturityRatings":[""],
+      "isDubbed":false,
+      "isSubbed":false,
+      "isMature":false,
+      "duration":"00:00",
+      "slug":"",
+      "premiered":"",
+      "episodeNavigation":{"prev":"","next":""},
+      "servers":[""]
+      }
+    ]);
+
+    
+    useEffect(() => {
+      fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(animeWatchLink)}&nocache=${Date.now()}`)
+      .then(response => {
+       if (response.ok) {
+        return response.json()
+      }
+        throw new Error('Network response was not ok.')
+      })
+      .then(data => {
+        var finalData = JSON.parse(data.contents);
+        setAnimeWatch(finalData);
+      });
+  
+    }, []);
+
   
     function ambientMode() {
       var btn = document.getElementById("btnAmbient");
@@ -35,7 +89,7 @@ function EpisodeView() {
             <div className='video-container'>
             <canvas id="canvas"></canvas>
                 {/* <video id="player" width="400" height="200" controls src=""> Browser not supported </video> */}
-                <iframe id='player' title="episode" allowFullScreen src="https://kaast1.com/sapphire-duck/player.php?src=MWJkODExMTJkZGI3OTRlY2I5MDgwYjVkYmY0ZWRmNzI6MDFmMDY1ODM5NWI4NDkyYzgwYmI5NmMzNTZlYTVmNWVkZTJhNzNjNGY2ZmIzZTMwNDkzOGRjMTdkZjBiNDQxZmM1MGY0MzZiNWU3YmM4ODdmMGJhNTA0ZmVmM2U4MTc0" ></iframe>
+                <iframe id='player' title="episode" allowFullScreen src={animeWatch.name!==""?animeWatch.servers:""} ></iframe>
                 {/* width="864" height="486" */}
             </div>
         </div>
@@ -44,8 +98,8 @@ function EpisodeView() {
         </div>
 
         <div className='title-container'>
-        <h2>SPY x FAMILY</h2>
-        <h5>Episode 1 - OPERATION STRIX (MISSION: 1)</h5>
+        <h2>{animeWatch.title}</h2>
+        <h5>Episode {animeWatch.episodeNumber} - {animeWatch.name}</h5>
                 
         </div>
         <div className='metadata-container'>
